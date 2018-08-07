35e3df3ecc368a7b115906253f8e854670539bd1
dmitrychaban <dmitrychaban@yahoo.com>
2018-08-07 17:59:02 +0300

    Initial Commit

---
+++ You're better than you think/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  You're better than you think
+//
+//  Created by User on 8/7/18.
+//  Copyright © 2018 Dmitry Chaban. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ You're better than you thinkTests/You_re_better_than_you_thinkTests.swift
@@ -0,0 +1,36 @@
+//
+//  You_re_better_than_you_thinkTests.swift
+//  You're better than you thinkTests
+//
+//  Created by User on 8/7/18.
+//  Copyright © 2018 Dmitry Chaban. All rights reserved.
+//
+
+import XCTest
+@testable import You_re_better_than_you_think
+
+class You_re_better_than_you_thinkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
